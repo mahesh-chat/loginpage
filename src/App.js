688e77f83c88d7578388c29daf9fbe9754d5d05c
mahesh-chat <maheshchatpatil@gmail.com>
2020-09-01 17:45:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import React from 'react';
+import Header from './components/Header/Header';
+import RegistrationForm from './components/RegistrationForm/RegistrationForm';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <div className="container d-flex align-items-center flex-column">
+      <RegistrationForm />
+       </div>
     </div>
-  );
+  )
 }
 
 export default App;
